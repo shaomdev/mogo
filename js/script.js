acf01272cc6698b6220d9039a56002caba58100b
Shamil Omarov <omarov_frontend@mail.ru>
2026-05-03 11:06:09 +0300

    added a interactiveness to the search

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Форма обратной связи
 const modal = document.getElementById('contactModal');
 const openBtn = document.querySelector('.nav__link--contact'); // Убедись, что у ссылки в меню есть такой класс
 const closeBtn = document.getElementById('closeModal');
@@ -17,5 +18,31 @@ closeBtn.addEventListener('click', () => {
 window.addEventListener('click', (e) => {
     if (e.target === modal) {
         modal.classList.remove('modal--open');
+    }
+});
+
+
+// поле поиска Search... добавил немного интерактивности
+const searchBtn = document.getElementById('searchBtn');
+const searchWrap = document.getElementById('searchWrap');
+const searchInput = document.querySelector('.search__input');
+
+searchBtn.addEventListener('click', (e) => {
+    // Если поле пустое и закрыто — открываем
+    if (!searchWrap.classList.contains('search--active')) {
+        e.preventDefault();
+        searchWrap.classList.add('search--active');
+        searchInput.focus();
+    } 
+    // Если поле открыто и там что-то написано — кнопка сработает как "Найти"
+    else if (searchInput.value === '') {
+        searchWrap.classList.remove('search--active');
+    }
+});
+
+// Закрываем, если кликнули в любое другое место сайта
+document.addEventListener('click', (e) => {
+    if (!searchWrap.contains(e.target)) {
+        searchWrap.classList.remove('search--active');
     }
 });
